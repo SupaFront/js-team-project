777f3c1b545df25aea761dd06b1e6788d8aa6606
Istomina Alena <mynikisalena@gmail.com>
2021-11-30 16:50:27 +0200

    добавляет закрытие модального окна

--- src/js/partials/modal.js
+++ src/js/partials/modal.js
@@ -4,25 +4,10 @@ import filmTemplate from '../../templates/modal-item.hbs';
 
 const galleryRef = document.querySelector('.film-gallery');
 const modalBackdropEl = document.querySelector('.modal-backdrop');
-const modalEl = document.querySelector('.modal-container');
 const closeModalBtn = document.querySelector('.modal-cls-btn');
 const cardContainerEl = document.querySelector('.card-container');
 const fetchFilm = new MoviesFetcher();
 const filmMarkup = new markupClass(cardContainerEl, filmTemplate, fetchFilm);
-
-// async function onCardClick() {}
-
-// function backdropOpenFn() {
-//   modalBackdropEl.classList.remove('is-hidden');
-// }
-
-// function backdropCloseFn() {
-//   modalBackdropEl.classList.add('is-hidden');
-// }
-
-// function renderFilmCardFn(object) {
-//   cardContainerEl.innerHTML = filmTemplate(object);
-// }
 
 galleryRef.addEventListener('click', e => {
   if (e.target.nodeName === 'IMG') {
@@ -39,8 +24,19 @@ async function fetchFilmById(id) {
 
 function openModal() {
   modalBackdropEl.classList.remove('is-hidden');
+  window.addEventListener('keydown', closeModalByEsc);
 }
 
 function closeModal() {
   modalBackdropEl.classList.add('is-hidden');
+  window.removeEventListener('keydown', closeModalByEsc);
+  filmMarkup.clearMarkup();
 }
+
+function closeModalByEsc(evt) {
+  if (evt.code === 'Escape') {
+    closeModal();
+  }
+}
+
+closeModalBtn.addEventListener('click', closeModal);
