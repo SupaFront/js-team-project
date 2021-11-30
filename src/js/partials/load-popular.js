464a60ee6247fc34590e42104e387349045e324a
fessenko-tr <fessenko.tr@gmail.com>
2021-11-30 10:30:01 +0200

    добавляет класс отрисовки интерфейса

--- src/js/partials/load-popular.js
+++ src/js/partials/load-popular.js
@@ -1,12 +1,14 @@
 import MoviesFetcher from './fetcher-class';
 import template from '/templates/grid-items.hbs';
+import markupClass from './markup-creator';
 
 const moviesFetcher = new MoviesFetcher();
 const galleryRef = document.querySelector('.film-gallery');
+const markupMaker = new markupClass(galleryRef, template, moviesFetcher);
 
 async function loadTrending() {
-  const arr = await moviesFetcher.getTrending();
-  galleryRef.insertAdjacentHTML('beforeend', template(arr));
+  const movieArray = await moviesFetcher.getTrending();
+  markupMaker.createMarkup('beforeend', movieArray);
 }
 
 loadTrending();
--- src/js/partials/search-btn.js
+++ src/js/partials/search-btn.js
@@ -1,24 +1,24 @@
 import MoviesFetcher from './fetcher-class';
 import template from '/templates/grid-items.hbs';
+import markupClass from './markup-creator';
 
-const searchBtnRef = document.querySelector('.btn');
-const userQuery = document.querySelector('input');
+const userQueryInput = document.querySelector('input');
 const formRef = document.querySelector('form');
 const galleryRef = document.querySelector('.film-gallery');
-
-const fetcherInstance = new MoviesFetcher();
+const moviesFetcher = new MoviesFetcher();
+const markupMaker = new markupClass(galleryRef, template, moviesFetcher);
 
 formRef.addEventListener('submit', e => {
   e.preventDefault();
-  fetcherInstance.queue = userQuery.value;
-  userQuery.value = '';
+  moviesFetcher.queue = userQueryInput.value;
+  userQueryInput.value = '';
   searchMovies();
 });
 
-fetcherInstance.openModal(312132);
-
 async function searchMovies() {
-  const arr = await fetcherInstance.searchMovie();
-  galleryRef.innerHTML = '';
-  galleryRef.insertAdjacentHTML('beforeend', template(arr)); // дальше рисовать маркап по этому объекту
+  const moviesArray = await moviesFetcher.searchMovie();
+  console.log(moviesArray);
+  markupMaker.clearMarkup();
+  markupMaker.createMarkup('beforeend', moviesArray);
+  // дальше рисовать маркап по этому объекту
 }
