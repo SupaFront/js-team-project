c48a5bd5f8c52f25800a52f9eb501d3821555807
fessenko-tr <fessenko.tr@gmail.com>
2021-12-01 15:53:25 +0200

    добавляет логику пагинатора для либы

--- src/js/partials/class-paginator.js
+++ src/js/partials/class-paginator.js
@@ -1,156 +1,176 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
+import { load } from './local-storage-functions';
 class Paginator {
-    constructor(container, markup, fetcher) {
-        this.container = container;
-        this.markup = markup;
-        this.fetcher = fetcher;
-    }
-    
-    
-    paginateTrending(hits) {
-        new Pagination(this.container, {
-totalItems: hits,
-     itemsPerPage: 20,
-     visiblePages: 5,
-     page: 1,
-     centerAlign: true,
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-    template: {
-        
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+  constructor(container, markup, fetcher, q) {
+    this.container = container;
+    this.markup = markup;
+    this.fetcher = fetcher;
+    this.q = q;
+  }
+
+  paginateTrending(hits) {
+    new Pagination(this.container, {
+      totalItems: hits,
+      itemsPerPage: 20,
+      visiblePages: 5,
+      page: 1,
+      centerAlign: true,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+      template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
         moveButton: ({ type }) => {
-             
+          let lastPage = hits / 20;
+          let template = ' ';
 
-            let lastPage = hits/20;
-            let template = ' ';
+          if (type === 'next') {
+            template = '<span>next</span>';
+          }
 
-            if (type === 'next') {
-                template =
-                  '<span>next</span>'
-                
-              }
+          if (type === 'prev') {
+            template = '<span>prev</span>';
+          }
 
-              if (type === 'prev') {
-                template =
-                  '<span>prev</span>'
-                
-              }
+          if (type === 'last') {
+            template = `<span class="inner-page-number">${lastPage}</span>`;
+          }
 
-              if (type === 'last') {
-                template =
-                  `<span class="inner-page-number">${lastPage}</span>`
-                
-              }
+          if (type === 'first') {
+            template = `<span class="inner-page-number">1</span>`;
+          }
 
+          return template;
+        },
+        disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</span>',
+        moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+          '<span class="tui-ico-ellip">...</span>' +
+          '</a>',
+      },
+    }).on('beforeMove', async evt => {
+      this.markup.clearMarkup();
+      this.fetcher.page = evt.page;
+      console.log(evt.page);
+      const movies = await this.fetcher.getTrending();
+      console.log(movies);
+      this.markup.createMarkup('beforeend', movies.results);
+    });
+  }
 
+  paginateSearch(hits) {
+    new Pagination(this.container, {
+      totalItems: hits,
+      itemsPerPage: 20,
+      visiblePages: 5,
+      page: 1,
+      centerAlign: true,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+      template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+        moveButton: ({ type }) => {
+          let lastPage = hits / 20;
+          let template = ' ';
 
-              if (type === 'first') {
-                  
-                template =
-                  `<span class="inner-page-number">1</span>`
-              }
+          if (type === 'next') {
+            template = '<span>next</span>';
+          }
 
+          if (type === 'prev') {
+            template = '<span>prev</span>';
+          }
 
+          if (type === 'last') {
+            template = `<span class="inner-page-number">${lastPage}</span>`;
+          }
 
-        
-            return template;
-          },
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
-}).on('beforeMove', async evt => {
-            this.markup.clearMarkup();
-            this.fetcher.page = evt.page;
-            console.log(evt.page)
-            const movies = await this.fetcher.getTrending();
-            console.log(movies)
-            this.markup.createMarkup('beforeend', movies.results);
+          if (type === 'first') {
+            template = `<span class="inner-page-number">1</span>`;
+          }
 
-        });
-    }
+          return template;
+        },
+        disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</span>',
+        moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+          '<span class="tui-ico-ellip">...</span>' +
+          '</a>',
+      },
+    }).on('beforeMove', async evt => {
+      this.markup.clearMarkup();
+      this.fetcher.page = evt.page;
+      console.log(evt.page);
+      const movies = await this.fetcher.searchMovie();
+      console.log(movies);
+      this.markup.createMarkup('beforeend', movies.results);
+    });
+  }
 
+  paginateLib(hits) {
+    new Pagination(this.container, {
+      totalItems: hits,
+      itemsPerPage: 20,
+      visiblePages: 5,
+      page: 1,
+      centerAlign: true,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+      template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+        moveButton: ({ type }) => {
+          let lastPage = hits / 20;
+          let template = ' ';
 
-    paginateSearch(hits) {
-        new Pagination(this.container, {
-            totalItems: hits,
-            itemsPerPage: 20,
-            visiblePages: 5,
-            page: 1,
-            centerAlign: true,
-            firstItemClassName: 'tui-first-child',
-            lastItemClassName: 'tui-last-child',
-            template: {
-        
-                page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-                currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-                moveButton: ({ type }) => {
-             
+          if (type === 'next') {
+            template = '<span>next</span>';
+          }
 
-                    let lastPage = hits / 20;
-                    let template = ' ';
+          if (type === 'prev') {
+            template = '<span>prev</span>';
+          }
 
-                    if (type === 'next') {
-                        template =
-                            '<span>next</span>'
-                
-                    }
+          if (type === 'last') {
+            template = `<span class="inner-page-number">${lastPage}</span>`;
+          }
 
-                    if (type === 'prev') {
-                        template =
-                            '<span>prev</span>'
-                
-                    }
+          if (type === 'first') {
+            template = `<span class="inner-page-number">1</span>`;
+          }
 
-                    if (type === 'last') {
-                        template =
-                            `<span class="inner-page-number">${lastPage}</span>`
-                
-                    }
-
-
-
-                    if (type === 'first') {
-                  
-                        template =
-                            `<span class="inner-page-number">1</span>`
-                    }
-
-
-
-        
-                    return template;
-                },
-                disabledMoveButton:
-                    '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                    '<span class="tui-ico-{{type}}">{{type}}</span>' +
-                    '</span>',
-                moreButton:
-                    '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                    '<span class="tui-ico-ellip">...</span>' +
-                    '</a>'
-            }
-        }).on('beforeMove', async evt => {
-            this.markup.clearMarkup();
-            this.fetcher.page = evt.page;
-            console.log(evt.page)
-            const movies = await this.fetcher.searchMovie();
-            console.log(movies)
-            this.markup.createMarkup('beforeend', movies.results);
-
-        });
-    }
-     
-
-
+          return template;
+        },
+        disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</span>',
+        moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+          '<span class="tui-ico-ellip">...</span>' +
+          '</a>',
+      },
+    }).on('beforeMove', async evt => {
+      console.log(this.q);
+      let perPage = evt.page * 20;
+      this.markup.clearMarkup();
+      const arr = [];
+      for (let i = perPage - 20; i < perPage; i++) {
+        if (load(this.q)[i] !== undefined) {
+          arr.push(load(this.q)[i]);
+        }
+      }
+      this.markup.createMarkup('beforeend', arr);
+    });
+  }
 }
 
-    export default Paginator;
+export default Paginator;
--- src/js/partials/load-local-storage.js
+++ src/js/partials/load-local-storage.js
@@ -2,25 +2,37 @@ import { save, load, remove } from './local-storage-functions';
 import filmTemplate from '/templates/grid-items.hbs';
 import MarkupCreator from './markup-creator';
 import MoviesFetcher from './fetcher-class';
+import Paginator from './class-paginator';
 
+const container = document.getElementById('pagination');
 const watchedBtn = document.querySelector('.watched-btn');
 const queueBtn = document.querySelector('.queue-btn');
-const galleryRef = document.querySelector('.film-gallery')
-const moviesFetcher = new MoviesFetcher()
+const galleryRef = document.querySelector('.film-gallery');
+const moviesFetcher = new MoviesFetcher();
 
-const markup = new MarkupCreator(galleryRef, filmTemplate, moviesFetcher)
+const markup = new MarkupCreator(galleryRef, filmTemplate, moviesFetcher);
 
+watchedBtn.addEventListener('click', event => {
+  markup.clearMarkup();
 
-    watchedBtn.addEventListener('click', (event) => {
-        markup.clearMarkup()
-        markup.createMarkup('beforeend', load('watched'))
-    })
-    queueBtn.addEventListener('click', (event) => {
-        markup.clearMarkup()
-        markup.createMarkup('beforeend', load('queue'))
+  if (load('watched') !== undefined) {
+    markup.createMarkup('beforeend', load('watched'));
+    const pagination = new Paginator(container, markup, moviesFetcher, 'watched');
+    pagination.paginateLib(load('watched').length);
+  }
+});
+queueBtn.addEventListener('click', event => {
+  markup.clearMarkup();
 
-    })
+  if (load('queue') !== undefined) {
+    markup.createMarkup('beforeend', load('queue'));
+    const pagination = new Paginator(container, markup, moviesFetcher, 'queue');
+    pagination.paginateLib(load('queue').length);
+  }
+});
 
-
-
-    markup.createMarkup('beforeend', load('watched'))
+if (load('watched') !== undefined) {
+  markup.createMarkup('beforeend', load('watched'));
+  const pagination = new Paginator(container, markup, moviesFetcher, 'watched');
+  pagination.paginateLib(load('watched').length);
+}
