aa02b87a2ca090a67b411d9b76e680e5e8f2d6f6
rostislavvolos <volosrostislavv@gmail.com>
2021-12-03 13:03:12 +0300

    1comm

--- src/js/partials/modal-footer.js
+++ src/js/partials/modal-footer.js
@@ -8,26 +8,45 @@
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+
+  const bodyEl = document.querySelector('body');
+  bodyEl.style.overflow = 'hidden';
+
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
+
+  // function closeModalByEsc(evt) {
+  //   if (evt.code === 'Escape') {
+  //     closeModal();
+  //   }
+  // }
+
+
+  // function closeModalByBackdrop(evt) {
+  //   if (!evt.target.classList.contains('modal-backdrop')) {
+  //     return;
+  //   }
+  //   closeModal();
+  // }
 })();
 
-const showDialog = () => {
-  document.getElementById('dialog').classList.add('show');
-  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-  const body = document.body;
-};
-const closeDialog = () => {
-  const body = document.body;
-  const scrollY = body.style.top;
-  body.style.position = '';
-  body.style.top = '';
-  window.scrollTo(0, parseInt(scrollY || '0') * -1);
-  document.getElementById('dialog').classList.remove('show');
-};
-window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
-});
 
-//hok
+// const showDialog = () => {
+//   document.getElementById('dialog').classList.add('show');
+//   const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+//   const body = document.body;
+// };
+// const closeDialog = () => {
+//   const body = document.body;
+//   const scrollY = body.style.top;
+//   body.style.position = '';
+//   body.style.top = '';
+//   window.scrollTo(0, parseInt(scrollY || '0') * -1);
+//   document.getElementById('dialog').classList.remove('show');
+// };
+// window.addEventListener('scroll', () => {
+//   document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+// });
+
+// //hok
